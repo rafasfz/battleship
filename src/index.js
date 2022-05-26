05ba0a36357cd0e78e6517b93add504e7b50e2ef
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-05-26 14:11:28 -0300

    add dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import app from './app.js'
 import lobby from './lobby.js'
 
-const server = app.listen(3000, () => console.log('Server running'))
+const server = app.listen(process.env.PORT || 3000, () =>
+  console.log('Server running')
+)
 
 lobby(server)
 
